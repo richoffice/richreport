be15962bf801d8b22ac4c6db993cb118c3c0c800
rocksun <daijun@gmail.com>
2022-07-01 16:50:28 +0800

    add date method

--- richreport_test.go
+++ richreport_test.go
@@ -16,6 +16,29 @@ func TestLoadDataFrames(t *testing.T) {
 
 }
 
+func TestLoadDateDataFrames(t *testing.T) {
+
+	got, err := LoadDataFrames("xlsx2map/testfiles/input.xlsx", "xlsx2map/testfiles/wh-def.json", nil)
+	if err != nil {
+		t.Errorf("LoadDataFrames() error = %v, want no error", err)
+		return
+	}
+	frame := got["workhours"]
+	fmt.Println(frame)
+	fmt.Println(frame.Col("dueDispatchTime").Str())
+
+	fmt.Println(frame.Col("dueDispatchTime").Records())
+
+	create, err := ParseDate(frame.Col("dueDispatchTime").Records()[0])
+	if err != nil {
+		t.Errorf("expected no error, but got %v", err)
+	}
+	fmt.Println(create)
+
+	// fmt.Println(got)
+
+}
+
 func TestStoreDataFrames(t *testing.T) {
 	got, err := LoadDataFrames("test/sample_file.xlsx", "test/sample_def.json", nil)
 	if err != nil {
